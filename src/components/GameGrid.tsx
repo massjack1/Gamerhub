3276993b61509385a8b4b941ed2fbe7fa1bff138
Mass Jack <mass.jack4@gmail.com>
2023-12-24 18:54:16 +0100

    Refactor: remove duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.ts";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
+import GameCardContainer from "./GameCardContainer.tsx";
 
 const GameGrid = () => {
     const  {games, error, isLoading} = useGames();
@@ -15,9 +16,14 @@ const GameGrid = () => {
             padding='10px'
             spacing={10}
         >
-            {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+            {isLoading && skeletons.map(skeleton =>
+                <GameCardContainer>
+                 <GameCardSkeleton key={skeleton} />
+                </GameCardContainer> )}
             {games.map((game) => (
+                <GameCardContainer>
                 <GameCard key={game.id} game={game}/>
+                </GameCardContainer>
             ))}
         </SimpleGrid>
         </>
